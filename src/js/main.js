075bcaa9aed58b6161640f8cd9555c30e98a7c5e
Ivan Olovyannikov <iolovyannikov@netlabsystems.com>
2019-09-16 14:54:27 +0500

    update partials css, validation, disable-button

--- src/js/main.js
+++ src/js/main.js
@@ -28,12 +28,50 @@ $(document).ready(function(){var $select1 = $('#tzone');
         }
     });
 
-    $('#profile-form').validate();
-
-    $('.form__label').each(function(){
-        if($(this).find('.form__label__input').hasClass('error') == true) {
-            $(this).parent().find('.form__label__title').addClass('error');
+    $('#profile-form').validate({
+        highlight: function(element) {
+            $(element).parent().addClass("form__label_error");
+        },
+        unhighlight: function(element) {
+            $(element).parent().removeClass("form__label_error");
         }
+    });
+
+
+    var orig = [];
+
+    $.fn.getType = function () {
+        return this[0].tagName == "INPUT" ? $(this[0]).attr("type").toLowerCase() : this[0].tagName.toLowerCase();
+    }
+
+    $("form :input").each(function () {
+        var type = $(this).getType();
+        var tmp = {
+            'type': type,
+            'value': $(this).val()
+        };
+        if (type == 'radio') {
+            tmp.checked = $(this).is(':checked');
+        }
+        orig[$(this).attr('id')] = tmp;
+    });
+
+    var button = $('#submit-settings');
+    $('#workspace-settings').bind('change keyup', function () {
+        var disable = true;
+        $("form :input").each(function () {
+            var type = $(this).getType();
+            var id = $(this).attr('id');
+            if (type == 'text' || type == 'select') {
+                disable = (orig[id].value == $(this).val());
+            } else if (type == 'radio') {
+                disable = (orig[id].checked == $(this).is(':checked'));
+            }
+            if (!disable) {
+                return false; // break out of loop
+            }
+        });
+        button.prop('disabled', disable);
     });
 
 });
